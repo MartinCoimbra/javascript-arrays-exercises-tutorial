4e54064522fa1c932742b964afac3d5589fb148a
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-20 20:23:24 +0000

    avances

--- exercises/10.1-Everything-is-awesome/app.js
+++ exercises/10.1-Everything-is-awesome/app.js
@@ -1,16 +1,16 @@
 let myArray = [1, 0, 0, 0, 1, 0, 0, 0, 1, 1];
-let j = 0;
+
 const ZerosToYahoos = (arr) => {
     let return_array = [];
+
     arr.forEach((item, index) => {
-        // magic goes inside these brackets
-        if (arr[item] == 1) {
-            return_array[j] = arr[item]
-            j++;
+        
+        if (item == 1) {
+            return_array[index] = arr[index]
         }else{
-            return_array[j] = 'Yahoo'
-            j++;
+            return_array[index] = 'Yahoo'
         }
+
     });
     
     return return_array;
